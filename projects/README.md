7274dde019d24bec69d58feb44bce16c0bf1b864
shumphries <seth.humphries@sdl.usu.edu>
2015-12-18 09:41:12 -0700

    start developing solution to get qt built

--- projects/README.md
+++ projects/README.md
@@ -2,3 +2,4 @@
 
 |project|license|description|version|repository|patch/diff|
 |-------|-------|-----------|-------|----------|----------|
+|[Qt](http://qt.io/ 'Qt - Home')|[lgpl](http://www.qt.io/qt-licensing-terms/ 'LGPL')|One Qt Code: Create Powerful Applications & Devices|v5.5.0|[repo](https://code.qt.io/qt/qt5 'Qt5 self hosted repo')|none|
--- projects/qt5.cmake
+++ projects/qt5.cmake
@@ -15,12 +15,13 @@ set(PRO_QT5
   DESC "One Qt Code: Create Powerful Applications & Devices"
   REPO "repo" ${REPO} "Qt5 self hosted repo"
   VER ${VER}
-  GIT_ORIGIN git://code.qt.io/qt/qt5.git
-  GIT_UPSTREAM git://code.qt.io/qt/qt5.git
-  # GIT_TAG xp-${VER_} # what to 'git checkout'
-  # GIT_REF hdf5-${VER_} # create patch from this tag to 'git checkout'
-  # DLURL ${REPO}/archive/${VER}.tar.gz
-  # DLMD5 3c0d7a8c38d1abc7b40fc12c1d5f2bb8
+  GIT_ORIGIN https://code.qt.io/qt/qt5.git
+  GIT_UPSTREAM https://code.qt.io/qt/qt5.git
+  GIT_TAG ${VER} # what to 'git checkout'
+  GIT_REF qt5-${VER} # create patch from this tag to 'git checkout'
+  DLNAME qt-everywhere-opensource-src-5.5.1.tar.gz
+  DLURL http://download.qt.io/official_releases/qt/5.5/5.5.1/single/${DLNAME}
+  DLMD5 59f0216819152b77536cf660b015d784
   # PATCH ${PATCH_DIR}/qt5.patch
   # DIFF ${REPO}/compare/
   )
@@ -37,9 +38,50 @@ function(patch_qt5)
   xpPatch(${PRO_QT5})
 endfunction()
 ########################################
+function(stringToList stringlist lvalue var)
+  if(NOT "${stringlist}" STREQUAL "")
+    string(STRIP ${stringlist} stringlist) # remove leading and trailing spaces
+    string(REPLACE " -" ";-" listlist ${stringlist})
+    foreach(item ${listlist})
+      list(APPEND templist ${lvalue}=${item})
+    endforeach()
+    set(${var} "${templist}" PARENT_SCOPE)
+  endif()
+endfunction()
+########################################
 function(build_qt5)
   if(NOT (XP_DEFAULT OR XP_PRO_QT5))
     return()
   endif()
-  xpCmakeBuild(qt5 "" "${XP_CONFIGURE}")
+  if(WIN32)
+    message(FATAL_ERROR "no windows yet")
+  else()
+    set(XP_CONFIGURE
+      -static
+      -qt-zlib
+      -qt-pcre
+      -qt-libpng
+      -qt-libjpeg
+      -qt-freetype
+      -qt-xcb
+      -qt-xkbcommon
+      -opengl desktop
+      -qt-sql-psql
+      -c++11
+      -opensource
+      -confirm-license
+      -make libs
+      -nomake examples
+      -nomake tools
+      -nomake tests
+      -skip qtwebchannel
+      -skip qtwebengine
+      -skip qtwebkit
+      -skip qtwebkit-examples
+      -skip qtenginio
+      -skip qt3d
+      )
+    xpCmakeBuild(qt5 "" "${XP_CONFIGURE}")
+  endif() # OS type
 endfunction()
+
