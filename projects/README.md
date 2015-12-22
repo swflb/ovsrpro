7a11abf17531e20aeaec8b4f71312ae8593c0c69
twaters <travis.waters@sdl.usu.edu>
2015-12-22 14:40:25 -0700

    zookeeper now working...still working on qt5...

--- projects/README.md
+++ projects/README.md
@@ -2,4 +2,5 @@
 
 |project|license|description|version|repository|patch/diff|
 |-------|-------|-----------|-------|----------|----------|
-|[Qt](http://qt.io/ 'Qt - Home')|[lgpl](http://www.qt.io/qt-licensing-terms/ 'LGPL')|One Qt Code: Create Powerful Applications & Devices|v5.5.0|[repo](https://code.qt.io/qt/qt5 'Qt5 self hosted repo')|none|
+|[Qt](http://qt.io/ 'Qt - Home')|[lgpl](http://www.qt.io/qt-licensing-terms/ 'LGPL')|One Qt Code: Create Powerful Applications & Devices|v5.5.0|[repo](http://code.qt.io/qt/qt5.git 'Qt5 main repo')|none|
+|[Zookeeper](https://zookeeper.apache.org/ 'Zookeeper - Home')|[Apache V2.0](http://www.apache.org/licenses/ 'Apache V2.0')|Apache ZooKeeper is an effort to develop and maintain an open-source server which enables highly reliable distributed coordination.|release-3.4.7|[repo](https://github.com/apache/zookeeper.git 'Zookeeper main repo')|none|
--- projects/qt5.cmake
+++ projects/qt5.cmake
@@ -6,107 +6,123 @@
 # build.
 ########################################
 xpProOption(qt5)
-set(VER v5.5.0)
-string(REPLACE "." "_" VER_ ${VER})
-set(REPO https://code.qt.io/qt/qt5)
-set(REPO_PATH ${CMAKE_BINARY_DIR}/xpbase/Source/qt5_repo)
-set(BUILD_PATH ${CMAKE_BINARY_DIR}/xpbase/Source/qt5)
+set(QT5_VER v5.5.0)
+set(QT5_REPO http://code.qt.io/qt/qt5.git)
+set(QT5_REPO_PATH ${CMAKE_BINARY_DIR}/xpbase/Source/qt5_repo)
+set(QT5_INSTALL_PATH ${CMAKE_BINARY_DIR}/xpbase/Install/qt5)
 set(PRO_QT5
   NAME qt5
   WEB "Qt" http://qt.io/ "Qt - Home"
   LICENSE "lgpl" http://www.qt.io/qt-licensing-terms/ "LGPL"
   DESC "One Qt Code: Create Powerful Applications & Devices"
-  REPO "repo" ${REPO} "Qt5 self hosted repo"
-  VER ${VER}
-  GIT_ORIGIN git://code.qt.io/qt/qt5.git
-  GIT_TAG ${VER} # what to 'git checkout'
-  )
-########################################
-function(mkpatch_qt5)
-  xpRepo(${PRO_QT5})
-  # Get all the submodules
-  ExternalProject_Add_Step(qt5_repo UpdateSubmodules
-    COMMAND ${GIT_EXECUTABLE} submodule update --init
-    COMMENT "Updating Qt5 submodules"
-    WORKING_DIRECTORY ${REPO_PATH})
-  # remove unwanted modules so they don't compile
-  if(WIN32)
-    set(DELETE_COMMAND "rmdir /Q /S")
-  else()
-    set(DELETE_COMMAND "rm -rf")
-  endif()
-  set(REMOVE_SUBMODULES
-    qtandroidextras
-    qtwebchannel
-    qtwebengine
-    qtwebkit
-    qtwebkit-examples)
-  foreach(submodule ${REMOVE_SUBMODULES})
-    ExternalProject_Add_Step(qt5_repo Delete${submodule}
-      COMMAND ${DELETE_COMMAND} ${submodule}
-      COMMENT "Deleting uneeded Qt5 submodules (${submodule})"
-      WORKING DIRECTORY ${REPO_PATH})
-  endforeach()
-endfunction()
-########################################
-function(download_qt5)
-  message("Download from git repo via mkpatch step")
-endfunction()
-########################################
-function(patch_qt5)
-  message("No Qt5 patch needed")
-endfunction()
-########################################
-function(build_qt5)
-  if(NOT (XP_DEFAULT OR XP_PRO_QT5))
-    return()
-  endif()
+  REPO "repo" ${QT5_REPO} "Qt5 main repo"
+  VER ${QT5_VER}
+  GIT_ORIGIN ${QT5_REPO}
+  GIT_TAG ${QT5_VER}
+)
+set(QT5_REMOVE_SUBMODULES
+  qtandroidextras
+  qtwebchannel
+  qtwebengine
+  qtwebkit
+  qtwebkit-examples)
+#######################################
+# setup the configure options
+macro(setConfigureOptions)
   # Define configure parameters
-  set(XP_CONFIGURE
-    "-qt-zlib"
-    "-qt-pcre"
-    "-qt-libpng"
-    "-qt-libjpeg"
-    "-qt-freetype"
-    "-opengl desktop"
-    #"-openssl"
-    "-qt-sql-psql"
-    "-qmake"
-    "-c++11"
-    "-opensource"
-    "-confirm-license"
-    "-make libs"
-    "-nomake examples"
-    "-nomake tools"
-    "-nomake tests"
-    "-prefix ${BUILD_PATH}")
+  set(QT5_CONFIGURE
+    -qt-zlib
+    -qt-pcre
+    -qt-libpng
+    -qt-libjpeg
+    -qt-freetype
+    -opengl desktop
+    #-openssl
+    -qt-sql-psql
+    -qmake
+    -opensource
+    -confirm-license
+    -make libs
+    -nomake examples
+    -nomake tools
+    -nomake tests
+    -prefix ${QT5_INSTALL_PATH})
   # Check whether to include debug build
   if(${XP_BUILD_DEBUG})
-    list(APPEND XP_CONFIGURE "-debug-and-release")
+    list(APPEND QT5_CONFIGURE -debug-and-release)
   else()
-    list(APPEND XP_CONFIGURE "-release")
+    list(APPEND QT5_CONFIGURE -release)
   endif()
   # Check if this is a static build
   if(${XP_BUILD_STATIC})
-    list(APPEND XP_CONFIGURE "-static")
+    list(APPEND QT5_CONFIGURE -static)
   endif()
   if(WIN32)
-    list(APPEND XP_CONFIGURE "-platform win32-msvc2013")
-    if(${XP_BUILD_STATIC})
-      # Copy the qmake conf file to setup the /MT compiler flag
-      file(COPY ${CMAKE_SOURCE_DIR}/qt5-msvc-desktop.conf
-           DESTINATION ${REPO_PATH}/qtbase/mkspecs/common/msvc-desktop.conf)
-    endif()
+    list(APPEND QT5_CONFIGURE -platform win32-msvc2013)
   else()
-    list(APPEND XP_CONFIGURE
-      "-platform linux-g++")
+    list(APPEND QT5_CONFIGURE -platform linux-g++ -c++11)
   endif() # OS type
+endmacro(setConfigureOptions)
+#######################################
+# Update the qmake conf with the /MT flag for static windows builds
+macro(setQtQmakeConf)
+  if(WIN32)
+    if(${XP_BUILD_STATIC})
+      # Copy the qmake conf file to setup the /MT compiler flag and enable
+      # multiple cores while compiling
+      configure_file(${CMAKE_SOURCE_DIR}/projects/qt5-msvc-desktop-mt.conf
+                     ${QT5_REPO_PATH}/qtbase/mkspecs/common/msvc-desktop.conf
+                     COPYONLY)
+    else()
+      # Copy the qmake conf file to setup the /MD compiler flag and enable
+      # multiple cores while compiling
+      configure_file(${CMAKE_SOURCE_DIR}/projects/qt5-msvc-desktop-md.conf
+                     ${QT5_REPO_PATH}/qtbase/mkspecs/common/msvc-desktop.conf
+                     COPYONLY)
+    endif()
+  endif()
+endmacro(setQtQmakeConf)
+#######################################
+# mkpatch_qt5 - initialize and clone the main repository
+function(mkpatch_qt5)
+
+endfunction(mkpatch_qt5)
+########################################
+# download - initialize the git submodules
+function(download_qt5)
+  # checkout the repository and update/init all submodules
+  ExternalProject_Add(qt5_repo
+    DOWNLOAD_DIR ${QT5_REPO_PATH}
+    GIT_REPOSITORY ${QT5_REPO}
+    GIT_TAG ${QT5_VER}
+    PATCH_COMMAND "" UPDATE_COMMAND "" CONFIGURE_COMMAND "" BUILD_COMMAND ""
+    INSTALL_COMMAND "")
+endfunction(download_qt5)
+########################################
+# patch - remove any of the unwanted submodules
+# so that they do not configure/compile
+function(patch_qt5)
+endfunction(patch_qt5)
+########################################
+# build - configure then build the libraries
+function(build_qt5)
+  setConfigureOptions()
+  setQtQmakeConf()
+
+  if(WIN32)
+    set(QT_BUILD_COMMAND "nmake")
+  else()
+    set(QT_BUILD_COMMAND "make")
+  endif()
+
+  ExternalProject_Add(qt5_build
+    DOWNLOAD_COMMAND "" UPDATE_COMMAND "" PATCH_COMMAND ""
+    SOURCE_DIR ${QT5_REPO_PATH}
+    CONFIGURE_COMMAND configure ${QT5_CONFIGURE}
+    BUILD_COMMAND ${QT_BUILD_COMMAND} BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ${QT_BUILD_COMMAND} install
+  )
+
 #  configure_file(${PRO_DIR}/use/usexp-qt5-config.cmake ${STAGE_DIR}/share/cmake/
 #  @ONLY NEWLINE_STYLE LF)
-  # Run Configure on the repository
-  ExternalProject_Add_Step(qt5_repo ConfigureQt5
-    COMMAND "configure ${XP_CONFIGURE}"
-    COMMENT "Configuring QT5"
-    WORKING_DIRECTORY ${REPO_PATH})
-  xpCmakeBuild(qt5 "" "{XP_CONFIGURE}")
-endfunction()
+endfunction(build_qt5)
