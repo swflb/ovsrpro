fbac39e340dc833bb0ffddb155fb08a51d1bc13e
TJ Gledhill <tj.gledhill@sdl.usu.edu>
2017-05-26 09:46:46 -0600

    Added Qt5 paths to the QtWebEngineProcess and the resources directory

--- projects/use/useop-qt5-config.cmake
+++ projects/use/useop-qt5-config.cmake
@@ -23,6 +23,8 @@ get_filename_component(OP_ROOTDIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 get_filename_component(OP_ROOTDIR ${OP_ROOTDIR} ABSOLUTE)
 set(QT5_BASE_PATH ${OP_ROOTDIR}/qt5)
 set(QT5_PLUGIN_PATH ${OP_ROOTDIR}/qt5/plugins)
+set(QT5_QTWEBENGINEPROCESS_PATH ${OP_ROOTDIR}/qt5/libexec/QtWebEngineProcess)
+set(QT5_RESOURCE_PATH ${OP_ROOTDIR}/qt5/resources)
 
 # find the Qt5 package
 # Note - This is not an exhaustive list, if new ones are needed add them here and
