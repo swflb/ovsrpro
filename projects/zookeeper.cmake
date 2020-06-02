e3d2ba988e9adcb1642163a3737a27a8fd89f077
brobeson <brobeson@users.noreply.github.com>
2020-06-02 09:02:53 -0600

    Upgrade ZooKeeper to 3.6.2
    - I upgraded ZooKeeper to 3.6.2.
    - I had to create a new patch file.
    - I had to change the build tools involved.
    - I removed the Windows patch.
    
    closes #61

--- projects/zookeeper.cmake
+++ projects/zookeeper.cmake
@@ -9,7 +9,7 @@ set(ZK_REPO https://github.com/apache/zookeeper)
 set(ZK_REPO_PATH ${CMAKE_BINARY_DIR}/xpbase/Source/zookeeper)
 set(ZK_SRC_PATH ${ZK_REPO_PATH}/src/c/src)
 set(ZK_INSTALL_PATH ${CMAKE_BINARY_DIR}/xpbase/Install/zookeeper)
-set(ZK_VER 3.4.13)
+set(ZK_VER 3.6.2)
 if(WIN32)
   set(ZOO_PATCH ${PATCH_DIR}/zookeeper-windows.patch)
 else()
@@ -25,7 +25,7 @@ set(PRO_ZOOKEEPER
   GIT_ORIGIN ${ZK_REPO}
   GIT_TAG release-${ZK_VER}
   DLURL ${ZK_REPO}/archive/release-${ZK_VER}.tar.gz
-  DLMD5 c6eeaab9624730f0a16a97dd24838813
+  DLMD5 f9cce4a74d28784fa1774ceb8ec52bf1
   DLNAME zookeeper-release-${ZK_VER}.tar.gz
   PATCH ${ZOO_PATCH} #This is only defined for Windows builds
 )
@@ -49,21 +49,20 @@ macro(zookeepercheckDependencies)
     message(FATAL_ERROR "javac required for zookeeper")
   endif()
 
-  find_program(ant ant)
-  mark_as_advanced(ant)
-  if(NOT ant)
-    message(FATAL_ERROR "ant required for zookeeper")
+  if(XP_DEFAULT OR XP_PRO_ZOOKEEPER)
+    find_package(Java REQUIRED COMPONENTS Development)
+    find_program(mvn mvn)
+    if(NOT mvn)
+      message(FATAL_ERROR "Maven is required to build zookeeper.")
+    endif()
   endif()
+
 endmacro()
 ########################################
 # build
 function(build_zookeeper)
   if(NOT (XP_DEFAULT OR XP_PRO_ZOOKEEPER))
     return()
-  endif()
-
-  if(NOT TARGET zookeeper)
-    xpPatchProject(${PRO_ZOOKEEPER})
   endif()
 
   zookeeperCheckDependencies()
@@ -73,101 +72,24 @@ function(build_zookeeper)
                  COPYONLY)
 
   ExternalProject_Get_Property(zookeeper SOURCE_DIR)
-  ExternalProject_Add(zookeeper_ant DEPENDS zookeeper
-    DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR}
+  ExternalProject_Add(
+    zookeeper_maven
+    DEPENDS zookeeper
+    DOWNLOAD_COMMAND ""
     SOURCE_DIR ${SOURCE_DIR}
-    CONFIGURE_COMMAND ant clean jar
-    BUILD_COMMAND ant compile_jute
-    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND mvn --batch-mode clean compile -DskipTests
+    BUILD_IN_SOURCE true
     INSTALL_COMMAND ""
   )
-
-  if(WIN32)
-    #TODO Still need to convert the Windows build to use ExternalProject...
-    set(ZK_INCLUDE_PATH ${SOURCE_DIR}/src/c/include)
-    set(zookeeper_hdr_files
-      ${ZK_INCLUDE_PATH}/proto.h
-      ${ZK_INCLUDE_PATH}/recordio.h
-      ${ZK_INCLUDE_PATH}/zookeeper.h
-      ${ZK_INCLUDE_PATH}/zookeeper_log.h
-      ${ZK_INCLUDE_PATH}/zookeeper_version.h
-      ${SOURCE_DIR}/src/c/generated/zookeeper.jute.h
-      ${SOURCE_DIR}/src/c/src/winport.h
-      ${ZK_INCLUDE_PATH}/winconfig.h
-      ${ZK_INCLUDE_PATH}/winstdint.h)
-
-    add_custom_target(zookeeper_build ALL
-      WORKING_DIRECTORY ${ZK_REPO_PATH}/src/c
-      COMMAND msbuild ${ZK_REPO_PATH}/src/c/zookeeper.sln /p:Configuration=Release\;Platform=x64 /t:zookeeper:rebuild
-      COMMAND msbuild ${ZK_REPO_PATH}/src/c/zookeeper.sln /p:Configuration=Debug\;Platform=x64 /t:zookeeper:rebuild
-      COMMAND ${CMAKE_COMMAND} -E make_directory ${STAGE_DIR}/include/zookeeper
-      COMMAND ${CMAKE_COMMAND} -E make_directory ${STAGE_DIR}/lib
-      COMMAND ${CMAKE_COMMAND} -E copy ${ZK_REPO_PATH}/src/c/x64/Debug/libzookeeperd-mt.lib ${STAGE_DIR}/lib/libzookeeperd-mt.lib
-      COMMAND ${CMAKE_COMMAND} -E copy ${ZK_REPO_PATH}/src/c/x64/Release/libzookeeper-mt.lib ${STAGE_DIR}/lib/libzookeeper-mt.lib
-      DEPENDS zookeeper zookeeper_ant
-    )
-
-    # copy the needed header files
-    foreach(hdr_file ${zookeeper_hdr_files})
-      get_filename_component(hdr_name ${hdr_file} NAME)
-      add_custom_command(TARGET zookeeper_build POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy ${hdr_file} ${STAGE_DIR}/include/zookeeper/${hdr_name}
-      )
-    endforeach()
-  else()
-    # This is only needed for non-windows build
-    xpDownloadProject(${CPP_UNIT})
-
-    set(ACLOCAL_STR "aclocal -I ${SOURCE_DIR}/src/c/cppunit-${CPP_UNIT_VER}")
-    ExternalProject_Add(zookeeper_configure DEPENDS zookeeper_ant download_cppunit-${CPP_UNIT_VER}.tar.gz
-      DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR}
-      SOURCE_DIR ${SOURCE_DIR}/src/c
-      CONFIGURE_COMMAND ${CMAKE_COMMAND} -E tar xzf ${DWNLD_DIR}/cppunit-${CPP_UNIT_VER}.tar.gz
-      BUILD_COMMAND ${CMAKE_COMMAND} -E env ACLOCAL=${ACLOCAL_STR} autoreconf -if -W none
-      BUILD_IN_SOURCE 1
-      INSTALL_COMMAND ""
-    )
-
-    ExternalProject_Add(zookeeper_Release DEPENDS zookeeper_configure
-      DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR}
-      SOURCE_DIR ${SOURCE_DIR}/src/c
-      CONFIGURE_COMMAND
-        ./configure
-        CFLAGS=-Wno-format-overflow
-        --without-cppunit
-        --prefix=${STAGE_DIR}
-      BUILD_COMMAND $(MAKE) clean && $(MAKE)
-      BUILD_IN_SOURCE 1
-      INSTALL_COMMAND $(MAKE) install
-    )
-
-    if(${XP_BUILD_DEBUG})
-      #TODO need to make zookeeper add a suffix so debug build can go in lib folder
-      ExternalProject_Add(zookeeper_Debug DEPENDS zookeeper_Release
-        DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR}
-        SOURCE_DIR ${SOURCE_DIR}/src/c
-        CONFIGURE_COMMAND
-          ./configure
-          CFLAGS=-Wno-format-overflow
-          --without-cppunit
-          --libdir=${STAGE_DIR}/lib/zookeeperDebug
-          --enable-debug
-        BUILD_COMMAND $(MAKE) clean && $(MAKE)
-        BUILD_IN_SOURCE 1
-        INSTALL_COMMAND $(MAKE) install-libLTLIBRARIES
-      )
-    endif()
-  endif()
-
-  # Copy LICENSE, NOTICE, and README files to STAGE_DIR
-  ExternalProject_Add(zookeeper_install_files DEPENDS zookeeper_Release
-    DOWNLOAD_COMMAND "" DOWNLOAD_DIR ${NULL_DIR}
-    SOURCE_DIR ${NULL_DIR} CONFIGURE_COMMAND "" BUILD_COMMAND ""
-    INSTALL_COMMAND
-      ${CMAKE_COMMAND} -E make_directory ${STAGE_DIR}/share/zookeeper &&
-      ${CMAKE_COMMAND} -E copy ${SOURCE_DIR}/src/c/LICENSE ${STAGE_DIR}/share/zookeeper &&
-      ${CMAKE_COMMAND} -E copy ${SOURCE_DIR}/src/c/NOTICE.txt ${STAGE_DIR}/share/zookeeper &&
-      ${CMAKE_COMMAND} -E copy ${SOURCE_DIR}/src/c/README ${STAGE_DIR}/share/zookeeper
+  set(
+    cmake_options
+    -DWANT_CPPUNIT:bool=off
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_INSTALL_INCLUDEDIR=include/zookeeper
+    # This must be OFF (case sensitive) to disable OpenSSL.
+    -DWITH_OPENSSL:STRING=OFF
+    -DCMAKE_DEBUG_POSTFIX=d
   )
-
+  xpCmakeBuild(zookeeper zookeeper_maven "${cmake_options}")
 endfunction(build_zookeeper)
